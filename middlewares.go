8d656f94f1286221f31f92221d12fae136b15c35
Joseph Sadden <jsadden@tantalus.com>
2019-11-13 12:31:15 -0800

    Added Protocol version to default logging middleware

--- middlewares.go
+++ middlewares.go
@@ -11,7 +11,7 @@ type MiddlewareHandler func(http.Handler) http.Handler
 // DefaultLoggingMiddleware provides a simple logging middleware, to view incoming connections as they arrive.
 func DefaultLoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Recieved [ %s ] Request for URL \"%s\" from Address: [ %s ].\n", r.Method, r.URL.String(), r.RemoteAddr)
+		log.Printf("Recieved [ %s ] [ %s ] Request for URL \"%s\" from Address: [ %s ].\n", r.Proto, r.Method, r.URL.String(), r.RemoteAddr)
 		next.ServeHTTP(w, r)
 	})
 }
