0e764903420f2bf7867ca1317e5b07ecb1a3974d
Joseph Sadden <jsadden6@gmail.com>
2019-11-14 15:57:07 -0800

    Added more default middlewares to include as necessary

--- middlewares.go
+++ middlewares.go
@@ -3,6 +3,7 @@ package easytls
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 // MiddlewareHandler represents the general format for a Middleware handler.
@@ -12,6 +13,27 @@ type MiddlewareHandler func(http.Handler) http.Handler
 func DefaultLoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Recieved [ %s ] [ %s ] Request for URL \"%s\" from Address: [ %s ].\n", r.Proto, r.Method, r.URL.String(), r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// LimitMaxConnectionMiddleware will provide a mechanism to strictly limit the maximum number of connections served.
+func LimitMaxConnectionMiddleware(next http.Handler, ConnectionLimit int) http.Handler {
+	semaphore := make(chan struct{}, ConnectionLimit)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		semaphore <- struct{}{}
+		defer func() { <-semaphore }()
+		next.ServeHTTP(w, r)
+	})
+}
+
+// LimitMaxConnectionRate will limit the server to a maximum of one connection per CycleTime.
+func LimitMaxConnectionRate(next http.Handler, CycleTime time.Duration) http.Handler {
+	ticker := time.NewTicker(CycleTime)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		<-ticker.C
 		next.ServeHTTP(w, r)
 	})
 }
