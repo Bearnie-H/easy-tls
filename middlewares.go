57bad3e4a0e888974efffef4e74f91647a9ff3c9
Joseph Sadden <jsadden6@gmail.com>
2019-11-16 12:23:34 -0800

    Added verbose toggle for connection limiting middlewares

--- middlewares.go
+++ middlewares.go
@@ -12,19 +12,22 @@ type MiddlewareHandler func(http.Handler) http.Handler
 // MiddlewareDefaultLogger provides a simple logging middleware, to view incoming connections as they arrive.
 func MiddlewareDefaultLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Recieved [ %s ] [ %s ] Request for URL \"%s\" from Address: [ %s ].\n", r.Proto, r.Method, r.URL.String(), r.RemoteAddr)
+		log.Printf("[MiddlewareDefaultLogger] Recieved [ %s ] [ %s ] Request for URL \"%s\" from Address: [ %s ].\n", r.Proto, r.Method, r.URL.String(), r.RemoteAddr)
 		next.ServeHTTP(w, r)
 	})
 }
 
 // MiddlewareLimitMaxConnections will provide a mechanism to strictly limit the maximum number of connections served.
-func MiddlewareLimitMaxConnections(ConnectionLimit int) func(http.Handler) http.Handler {
+func MiddlewareLimitMaxConnections(ConnectionLimit int, verbose bool) func(http.Handler) http.Handler {
 	semaphore := make(chan struct{}, ConnectionLimit)
 
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			semaphore <- struct{}{}
 			defer func() { <-semaphore }()
+			if verbose {
+				log.Printf("[MiddlewareLimitMaxConnections] Processing [ %s ] [ %s ] Request for URL \"%s\" from Address: [ %s ].\n", r.Proto, r.Method, r.URL.String(), r.RemoteAddr)
+			}
 			h.ServeHTTP(w, r)
 		})
 	}
@@ -32,12 +35,15 @@ func MiddlewareLimitMaxConnections(ConnectionLimit int) func(http.Handler) http.
 }
 
 // MiddlewareLimitConnectionRate will limit the server to a maximum of one connection per CycleTime.
-func MiddlewareLimitConnectionRate(CycleTime time.Duration) func(http.Handler) http.Handler {
+func MiddlewareLimitConnectionRate(CycleTime time.Duration, verbose bool) func(http.Handler) http.Handler {
 	ticker := time.NewTicker(CycleTime)
 
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			<-ticker.C
+			if verbose {
+				log.Printf("[MiddlewareLimitConnectionRate] Processing [ %s ] [ %s ] Request for URL \"%s\" from Address: [ %s ].\n", r.Proto, r.Method, r.URL.String(), r.RemoteAddr)
+			}
 			h.ServeHTTP(w, r)
 		})
 	}
