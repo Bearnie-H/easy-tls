0010322def142a7b63f13308033dc4d70be4fa43
Joseph Sadden <jsadden@tantalus.com>
2019-12-13 15:22:48 -0800

    Rearranged, ignoring leading slash on routing rule path match function

--- proxy/matcherFuncs.go
+++ proxy/matcherFuncs.go
@@ -11,20 +11,29 @@ import (
 // ReverseProxyRouterFunc represents the Type which must be satisfied by any function which defines the per-request routing behaviours.  This must map a given request to a specific IP:Port host.
 type ReverseProxyRouterFunc func(*http.Request) (string, error)
 
-// ReverseProxyRoutingRule implements a single routing rule to be followed by the Reverse Proxy when re-routing traffic.  This will take in a URL path, and return the Host:Port/Path to forward the corresponding request to.  This implementation is very basic, effectively effectively just re-routing based on the
+// ReverseProxyRoutingRule implements a single routing rule to be followed by the Reverse Proxy when re-routing traffic.  This will take in a URL path, and return the Host:Port to forward the corresponding request to.  This implementation is very basic, effectively effectively just re-routing to a new Host:Port based on the Path Prefix.
 type ReverseProxyRoutingRule struct {
 	PathPrefix      string
 	DestinationHost string
 	DestinationPort int
 }
 
-// Simple matching function, abstracted away to allow the "Rules" to become more complex as this library develops.
-func (R *ReverseProxyRoutingRule) matches(Path string) bool {
-	return strings.HasPrefix(Path, R.PathPrefix)
-}
-
 // reverseProxyRoutingRuleSet aliases an iterable set of routing rules.
 type reverseProxyRoutingRuleSet []ReverseProxyRoutingRule
+
+// Simple matching function, abstracted away to allow the "Rules" to become more complex as this library develops.
+func (R *ReverseProxyRoutingRule) matches(Path string) bool {
+
+	if !strings.HasPrefix(Path, "/") {
+		Path = "/" + Path
+	}
+
+	if !strings.HasPrefix(R.PathPrefix, "/") {
+		R.PathPrefix = "/" + R.PathPrefix
+	}
+
+	return strings.HasPrefix(Path, R.PathPrefix)
+}
 
 // LiveFileRouter implements a Reverse Proxy Routing function which will follow rules defined in a JSON file on disk. This rule-set is consulted on each incoming request,
 func LiveFileRouter(RulesFilename string) ReverseProxyRouterFunc {
