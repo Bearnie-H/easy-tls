2f45276b1ac67b40e74236c5580f0d45b68395ce
Joseph Sadden <jsadden6@gmail.com>
2020-01-19 11:46:07 -0800

    Added HTTPS -> HTTP downgrade retry logic, and it works for nil/empty request bodies

--- client/client-methods.go
+++ client/client-methods.go
@@ -6,13 +6,25 @@ import (
 	"mime/multipart"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // ErrInvalidStatusCode references the Error returned by any request which succeeds in communicating with the server, but does not return a 2xx level response code.
 var ErrInvalidStatusCode = errors.New("Invalid status code - Expected 2xx")
 
+func (C *SimpleClient) setScheme(URL *url.URL) {
+	if C.IsTLS() {
+		URL.Scheme = "https"
+	} else {
+		URL.Scheme = "http"
+	}
+}
+
 // Get is the wrapper function for an HTTP "GET" request. This will create a GET request with an empty body, and the specified headers. The header map can be set to nil if no additional headers are required. This function returns an error and nil response on an HTTP StatusCode which is outside the 200 block.
 func (C *SimpleClient) Get(URL *url.URL, Headers map[string][]string) (*http.Response, error) {
+
+	// Set the scheme, allowing for this to be missing, and be able to be defined by the internal TLS state of the Client.
+	C.setScheme(URL)
 
 	// Create the request
 	req, err := NewRequest(http.MethodGet, URL, Headers, nil)
@@ -26,6 +38,9 @@ func (C *SimpleClient) Get(URL *url.URL, Headers map[string][]string) (*http.Res
 
 // Head is the wrapper function for an HTTP "HEAD" request. This will create a new HEAD request with an empty body and the specified headers. The header map can be set to nil if no additional headers are required. This will ONLY return the HTTP Response Header map from the server. The overall Response Body (if it exists) will be closed by this function. This function returns an error and nil Header on an HTTP StatusCode which is outside the 200 block.
 func (C *SimpleClient) Head(URL *url.URL, Headers map[string][]string) (*http.Response, error) {
+
+	// Set the scheme, allowing for this to be missing, and be able to be defined by the internal TLS state of the Client.
+	C.setScheme(URL)
 
 	// Create the request
 	req, err := NewRequest(http.MethodHead, URL, Headers, nil)
@@ -52,6 +67,9 @@ func (C *SimpleClient) Head(URL *url.URL, Headers map[string][]string) (*http.Re
 // This function "may" support MultiPart POST requests, by way of io.Pipes and multipart.Writers, but this has not been tested, and multipart Post is planned to be an explicit function in the future.
 func (C *SimpleClient) Post(URL *url.URL, Contents io.Reader, Headers map[string][]string) (*http.Response, error) {
 
+	// Set the scheme, allowing for this to be missing, and be able to be defined by the internal TLS state of the Client.
+	C.setScheme(URL)
+
 	// Create the request
 	req, err := NewRequest(http.MethodPost, URL, Headers, Contents)
 	if err != nil {
@@ -72,6 +90,9 @@ func (C *SimpleClient) PostMultipart(URL *url.URL, Contents multipart.Reader, He
 // Put is the wrapper function for an HTTP "PUT" request. This will create a new PUT request with a body composed of the contents of the io.Reader passed in, and the specified headers. The header map can be set to nil if no additional headers are required. If a nil ReadCloser is passed in, this will create an empty Put body which is allowed. This will return the full HTTP Response from the server, unaltered. This function returns an error and nil response on an HTTP StatusCode which is outside the 200 block.
 func (C *SimpleClient) Put(URL *url.URL, Contents io.Reader, Headers map[string][]string) (*http.Response, error) {
 
+	// Set the scheme, allowing for this to be missing, and be able to be defined by the internal TLS state of the Client.
+	C.setScheme(URL)
+
 	// Create the request
 	req, err := NewRequest(http.MethodPut, URL, Headers, Contents)
 	if err != nil {
@@ -85,6 +106,9 @@ func (C *SimpleClient) Put(URL *url.URL, Contents io.Reader, Headers map[string]
 // Delete is the wrapper function for an HTTP "DELETE" request. This will create a new DELETE request with an empty body, and the specified headers. The header map can be set to nil if no additional headers are required.
 // This will return ONLY an error, and no HTTP Response components. The internal HTTP Response from the server will be safely closed by this function.
 func (C *SimpleClient) Delete(URL *url.URL, Headers map[string][]string) (*http.Response, error) {
+
+	// Set the scheme, allowing for this to be missing, and be able to be defined by the internal TLS state of the Client.
+	C.setScheme(URL)
 
 	// Create the request
 	req, err := NewRequest(http.MethodDelete, URL, Headers, nil)
@@ -109,6 +133,9 @@ func (C *SimpleClient) Delete(URL *url.URL, Headers map[string][]string) (*http.
 // Patch is the wrapper function for an HTTP "PATCH" request. This will create a new PATCH request with a body composed of the contents of the io.Reader passed in, and the specified headers. The header map can be set to nil if no additional headers are required. If a nil ReadCloser is passed in, this will create an empty Patch body which is allowed. This will return the full HTTP Response from the server, unaltered. This function returns an error and nil response on an HTTP StatusCode which is outside the 200 block.
 func (C *SimpleClient) Patch(URL *url.URL, Contents io.Reader, Headers map[string][]string) (*http.Response, error) {
 
+	// Set the scheme, allowing for this to be missing, and be able to be defined by the internal TLS state of the Client.
+	C.setScheme(URL)
+
 	// Create the request
 	req, err := NewRequest(http.MethodPatch, URL, Headers, Contents)
 	if err != nil {
@@ -127,6 +154,9 @@ func (C *SimpleClient) Connect(URL *url.URL, Headers map[string][]string) error 
 // Options is the wrapper function for an HTTP "OPTIONS" request. This will create a new OPTIONS request with an empty body, and the specified headers. The header map can be set to nil if no additional headers are required. This will return the full HTTP Response from the server, unaltered. This function returns an error and nil response on an HTTP StatusCode which is outside the 200 block.
 func (C *SimpleClient) Options(URL *url.URL, Headers map[string][]string) (*http.Response, error) {
 
+	// Set the scheme, allowing for this to be missing, and be able to be defined by the internal TLS state of the Client.
+	C.setScheme(URL)
+
 	// Create the request
 	req, err := NewRequest(http.MethodOptions, URL, Headers, nil)
 	if err != nil {
@@ -139,6 +169,9 @@ func (C *SimpleClient) Options(URL *url.URL, Headers map[string][]string) (*http
 
 // Trace is the wrapper function for an HTTP "TRACE" request. This will create a new TRACE request with an empty body, and the specified headers. The header map can be set to nil if no additional headers are required. This will return the full HTTP Response from the server, unaltered. This function returns an error and nil response on an HTTP StatusCode which is outside the 200 block.
 func (C *SimpleClient) Trace(URL *url.URL, Headers map[string][]string) (*http.Response, error) {
+
+	// Set the scheme, allowing for this to be missing, and be able to be defined by the internal TLS state of the Client.
+	C.setScheme(URL)
 
 	// Create the request
 	req, err := NewRequest(http.MethodTrace, URL, Headers, nil)
@@ -156,15 +189,51 @@ func (C *SimpleClient) Trace(URL *url.URL, Headers map[string][]string) (*http.R
 // This will perform no alterations to the provided request, and no alterations to the returned Response.
 // This function ErrInvalidStatusCode and the full response on an HTTP StatusCode which is outside the 200 block.
 // This is still a meaningful response, but a helpful error to quickly disambiguate errors of transport versus errors of action.
+// This function is extended by the use of a TLSRetryPolicy within the SimpleClient.  This allows a client to attempt to handle HTTP/HTTPS mismatch errors automatically by upgrading/downgrading as necessary.
 func (C *SimpleClient) Do(req *http.Request) (*http.Response, error) {
+
 	resp, err := C.client.Do(req)
 	if err != nil {
-		return nil, err
+		return C.shouldDowngrade(req, resp, err)
 	}
 
 	if 200 <= resp.StatusCode && resp.StatusCode < 300 {
 		return resp, nil
 	}
 
-	return resp, ErrInvalidStatusCode
+	return C.shouldUpgrade(req, resp, ErrInvalidStatusCode)
+}
+
+// Check if the client should downgrade to HTTP and attempt to perform the request again.
+func (C *SimpleClient) shouldDowngrade(req *http.Request, resp *http.Response, respErr error) (*http.Response, error) {
+
+	// If the Client Retry policy indicates either no retries, or only upgrades, don't attempt anything
+	if C.policy == NoRetry ||
+		C.policy == UpgradeNoReset ||
+		C.policy == UpgradeWithReset {
+		return resp, respErr
+	}
+
+	// Check if we have a standard golang http package error indicating an HTTP response for HTTPS client.
+	if strings.Contains(respErr.Error(), "http: server gave HTTP response to HTTPS client") {
+		return C.retryWithDowngrade(req, resp)
+	}
+
+	// Perform more, or other checks...
+
+	return resp, respErr
+}
+
+// Check if the client should try to upgrade to HTTPS and attempt to perform the request again.
+func (C *SimpleClient) shouldUpgrade(req *http.Request, resp *http.Response, respErr error) (*http.Response, error) {
+
+	if C.policy == NoRetry ||
+		C.policy == DowngradeNoReset ||
+		C.policy == DowngradeWithReset {
+		return resp, respErr
+	}
+
+	// if resp.StatusCode == http.StatusBadRequest
+
+	return resp, respErr
 }
--- client/client.go
+++ client/client.go
@@ -2,6 +2,8 @@ package client
 
 import (
 	"crypto/tls"
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -11,22 +13,48 @@ import (
 	"github.com/Bearnie-H/easy-tls/header"
 )
 
+// TLSRetryPolicy defines the possible policies that a SimpleClient can take when it communicates with the wrong HTTP/HTTPS to the server
+type TLSRetryPolicy int
+
+// Enum definition of the available TLSRetryPolicy values
+const (
+	NoRetry            TLSRetryPolicy = iota // Don't retry the attempt using a different Scheme
+	DowngradeNoReset                         // Attempt only downgrading from HTTPS to HTTP, and don't reset the SimpleClient after the request.
+	DowngradeWithReset                       // Attempt only downgrading from HTTPS to HTTP, and reset the SimpleClient after the request.
+	UpgradeNoReset                           // Attempt only upgrading from HTTP to HTTPS, but reset the SimpleClient to HTTP after the request.
+	UpgradeWithReset                         // Attempt only upgrading from HTTP to HTTPS, and keep the SimpleClient configured for HTTPS after the request.
+	SwapNoReset                              // Attempt to swap HTTP <-> HTTPS, and don't reset the SimpleClient after.
+	SwapWithReset                            // Attempt to swap HTTP <-> HTTPS, and do reset the SimpleClient after.
+)
+
 // SimpleClient is the primary object of this library.  This is the implementation of the simplified HTTP Client provided by this package.  The use and functionality of this is opaque to whether or not this is running in HTTP or HTTPS mode, with a basic utility function to check.
 type SimpleClient struct {
 	client *http.Client
+
 	tls    bool
+	bundle easytls.TLSBundle
+
+	policy TLSRetryPolicy
 }
 
 // NewClientHTTP will fully initialize a SimpleClient with TLS settings turned off.  These settings CAN be turned on and off as required.
-func NewClientHTTP() (*SimpleClient, error) {
-	return NewClientHTTPS(nil)
+func NewClientHTTP(TLSPolicy TLSRetryPolicy) (*SimpleClient, error) {
+	if TLSPolicy != NoRetry {
+		return nil, errors.New("easytls client error: Cannot initialize HTTP Client with TLS Retry Policy. Use NewClientHTTPS() or NoRetry instead")
+	}
+	return NewClientHTTPS(nil, TLSPolicy)
 }
 
 // NewClientHTTPS will fully initialize a SimpleClient with TLS settings turned on.  These settings CAN be turned on and off as required.
-func NewClientHTTPS(TLS *easytls.TLSBundle) (*SimpleClient, error) {
+func NewClientHTTPS(TLS *easytls.TLSBundle, TLSPolicy TLSRetryPolicy) (*SimpleClient, error) {
 	tls, err := easytls.NewTLSConfig(TLS)
 	if err != nil {
 		return nil, err
+	}
+
+	var saveBundle easytls.TLSBundle
+	if TLS != nil {
+		saveBundle = *TLS
 	}
 
 	s := &SimpleClient{
@@ -36,7 +64,9 @@ func NewClientHTTPS(TLS *easytls.TLSBundle) (*SimpleClient, error) {
 				TLSClientConfig:   tls,
 				ForceAttemptHTTP2: true,
 			}},
-		tls: !(TLS == nil),
+		tls:    !(TLS == nil),
+		bundle: saveBundle,
+		policy: TLSPolicy,
 	}
 
 	return s, nil
@@ -49,6 +79,7 @@ func (C *SimpleClient) IsTLS() bool {
 
 // NewRequest will create a new HTTP Request, ready to be used by any implementation of an http.Client
 func NewRequest(Method string, URL *url.URL, Headers http.Header, Contents io.Reader) (*http.Request, error) {
+
 	req, err := http.NewRequest(Method, URL.String(), Contents)
 	if err != nil {
 		return nil, err
@@ -60,16 +91,29 @@ func NewRequest(Method string, URL *url.URL, Headers http.Header, Contents io.Re
 }
 
 // EnableTLS will enable the TLS settings for a SimpleClient based on the provided TLSBundle.
-func (C *SimpleClient) EnableTLS(TLS *easytls.TLSBundle) error {
-	tls, err := easytls.NewTLSConfig(TLS)
+// If the client previously had a TLS bundle provided, this will fall back and attempt to use it
+func (C *SimpleClient) EnableTLS(TLS ...*easytls.TLSBundle) (err error) {
+
+	var tlsConf *tls.Config
+	for _, tls := range TLS {
+		tlsConf, err = easytls.NewTLSConfig(tls)
+		if err != nil {
+			continue
+		}
+	}
+
+	// If all of the provided TLSBundles failed, check if the Client has a saved one and use it.
 	if err != nil {
-		return err
+		tlsConf, err = easytls.NewTLSConfig(&C.bundle)
+		if err != nil {
+			return err
+		}
 	}
 
 	C.client = &http.Client{
 		Timeout: time.Hour,
 		Transport: &http.Transport{
-			TLSClientConfig:   tls,
+			TLSClientConfig:   tlsConf,
 			ForceAttemptHTTP2: true,
 		},
 	}
@@ -88,4 +132,93 @@ func (C *SimpleClient) DisableTLS() {
 		},
 	}
 	C.tls = false
+}
+
+// retryWithDowngrade will attempt to re-send an HTTP request, after downgrading from HTTPS to HTTP.
+// If this errors out, simply return the original response back, as if this never happpened.
+func (C *SimpleClient) retryWithDowngrade(r *http.Request, original *http.Response) (resp *http.Response, err error) {
+
+	// Check the Upgrade/Downgrade policy
+	switch C.policy {
+
+	// If the policy implies resetting the TLS status, defer that to happen at the end
+	case DowngradeWithReset, SwapWithReset:
+		defer func(err *error) {
+			err2 := C.EnableTLS()
+			if *err != nil {
+				if err2 != nil {
+					*err = fmt.Errorf("%s. easytls client error: Failed to re-enable TLS - %s", *err, err2)
+				}
+			}
+		}(&err) // Allow the reset of the TLS settings to report it's error back
+
+	// If the policy doesn't specify resetting, don't defer resetting it
+	case DowngradeNoReset, SwapNoReset:
+
+		// If the policy is anything else, we shouldn't have gotten here, but just return and ignore this call.
+	default:
+		return original, nil
+	}
+
+	C.DisableTLS()
+
+	newURL := *r.URL
+	newURL.Scheme = "http"
+
+	rewoundBody, err := C.rewindRequestBody(r)
+	if err != nil {
+		return original, fmt.Errorf("easytls client error: Failed to rewind request body in [ %s ] request to [ %s ] - %s", r.Method, r.URL.String(), err)
+	}
+
+	newReq, err := NewRequest(r.Method, &newURL, r.Header, rewoundBody)
+	if err != nil {
+		return original, fmt.Errorf("easytls client error: Failed to re-create [ %s ] request during TLS upgrade retry - %s", r.Method, err)
+	}
+
+	return C.Do(newReq)
+}
+
+// retryWithUpgrade will attempt to re-send an HTTP request, after upgrading from HTTP to HTTPS.
+// If this errors out, simply return the original response back, as if this never happpened.
+func (C *SimpleClient) retryWithUpgrade(r *http.Request, original *http.Response) (*http.Response, error) {
+
+	// Check the Upgrade/Downgrade policy
+	switch C.policy {
+
+	// If the policy implies resetting the TLS status, defer that to happen at the end
+	case UpgradeWithReset, SwapWithReset:
+		defer C.DisableTLS()
+
+	// If the policy doesn't specify resetting, don't defer resetting it
+	case UpgradeNoReset, SwapNoReset:
+
+		// If the policy is anything else, we shouldn't have gotten here, but just return and ignore this call.
+	default:
+		return original, nil
+	}
+
+	if err := C.EnableTLS(); err != nil {
+		return original, fmt.Errorf("easytls client error: Failed to enable TLS during retry attempt - %s", err)
+	}
+
+	newURL := *r.URL
+	newURL.Scheme = "https"
+
+	rewoundBody, err := C.rewindRequestBody(r)
+	if err != nil {
+		return original, fmt.Errorf("easytls client error: Failed to rewind request body in [ %s ] request to [ %s ] - %s", r.Method, r.URL.String(), err)
+	}
+
+	newReq, err := NewRequest(r.Method, &newURL, r.Header, rewoundBody)
+	if err != nil {
+		return original, fmt.Errorf("easytls client error: Failed to re-create [ %s ] request during TLS upgrade retry - %s", r.Method, err)
+	}
+
+	return C.Do(newReq)
+}
+
+// This needs to be updated to work for non-empty request bodies.
+func (C *SimpleClient) rewindRequestBody(r *http.Request) (io.ReadCloser, error) {
+	// NOTE: This needs to be implemented.  This should either attempt to rewind the request body if possible, or return a meaningful error.
+	return r.Body, nil
 }
